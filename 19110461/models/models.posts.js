24238e88d361870afc20686e70337c3de9328e74
Thang Duong <thangduong@futurify.vn>
2022-10-04 23:16:19 +0700

    feat: first commit

---
+++ 19110461/models/models.posts.js
@@ -0,0 +1,25 @@
+var posts = [{
+    id: 0,
+    title: 'Test post 01',
+    content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    comments: [{
+        user: 'ceritna',
+        comment: 'Duong Duc Thang - 19110461'
+    }, {
+        user: 'asta',
+        comment: 'where could i find them ?'
+    }]
+}, {
+    id: 1,
+    title: 'Vacancy',
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ipsum risus, facilisis quis magna eu, aliquam ornare tellus. Quisque in diam nec nibh fermentum lobortis sed eget massa.`,
+    comments: [{
+        user: 'Thang123',
+        comment: 'is there any left?'
+    }, {
+        user: 'Thangnef',
+        comment: `i can't connect to you`
+    }]
+}]
+
+module.exports = posts
